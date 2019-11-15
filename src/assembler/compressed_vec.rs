c7d5e419f41521b8c70584e02c5f1589584495f9
Dillon Hammond <dillonhammond@gmail.com>
2019-11-15 16:09:00 -0600

    start define compressedvec

--- src/assembler/compressed_vec.rs
+++ src/assembler/compressed_vec.rs
@@ -1,1 +1,35 @@
-pub struct CompressedVec<T> {}
+use std::{fmt::Debug, num::NonZeroUsize};
+
+#[derive(Clone, Debug)]
+enum Entry<T>
+where
+    T: Clone + Debug,
+{
+    Data(T),
+    Ptr(Compressed<T>),
+}
+
+#[derive(Clone, Debug)]
+struct Compressed<T>
+where
+    T: Clone + Debug,
+{
+    len: NonZeroUsize,
+    data: T,
+}
+
+impl<T> Compressed<T> where T: Clone + Debug {
+    pub fn new(data: T, len: NonZeroUsize) -> Compressed<T> {
+        Compressed { data, len }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CompressedVec<T>
+where
+    T: Clone + Debug,
+{
+    data: Vec<Entry<T>>,
+}
+
+impl<T> CompressedVec<T> where T: Clone + Debug {}
