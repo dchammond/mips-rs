39c4ac51d29f2430119026850d49e56317bc5759
Dillon Hammond <dillonhammond@gmail.com>
2020-01-10 01:03:11 -0600

    added constants for mips memory mapy

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -11,6 +11,35 @@ use std::{collections::HashMap, num::NonZeroU32};
 
 pub struct Assembled {}
 
+// grows down
+const TOP_RESERVED_SIZE: u32 = 0x0000_FFEF;
+const TOP_RESERVED_START: u32 = 0xFFFF_FFFF;
+const TOP_RESERVED_END: u32 = TOP_RESERVED_START - TOP_RESERVED_SIZE;
+
+const MMIO_SIZE: u32 = 0x0000_0010;
+const MMIO_START: u32 = TOP_RESERVED_END;
+const MMIO_END: u32 = MMIO_START - MMIO_SIZE;
+
+const KERNEL_DATA_SIZE: u32 = 0x6FFF_0000;
+const KERNEL_DATA_START: u32 = MMIO_END;
+const KERNEL_DATA_END: u32 = KERNEL_DATA_START - KERNEL_DATA_SIZE;
+
+const KERNEL_TEXT_SIZE: u32 = 0x1000_0000;
+const KERNEL_TEXT_START: u32 = KERNEL_DATA_END;
+const KERNEL_TEXT_END: u32 = KERNEL_TEXT_START - KERNEL_TEXT_SIZE;
+
+const STACK_START: u32 = KERNEL_TEXT_END;
+// grows up
+const STATIC_DATA_START: u32 = 0x1000_0000;
+
+const TEXT_SIZE: u32 = 0x0600_0000;
+const TEXT_END: u32 = STATIC_DATA_START;
+const TEXT_START: u32 = TEXT_END - TEXT_SIZE;
+
+const BOTTOM_RESERVED_SIZE: u32 = 0x0400_0000;
+const BOTTOM_RESERVED_END: u32 = TEXT_START;
+const BOTTOM_RESERVED_START: u32 = BOTTOM_RESERVED_END - BOTTOM_RESERVED_SIZE;
+
 /*
 fn expand_pseudo(text_segments: &mut [TextSegment]) {
     let insertions: Vec<((usize, usize), (Inst, Inst))> = Vec::new();
