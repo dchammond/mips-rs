2e9709ba0226162e4affd44719b9f60d74a94148
Dillon Hammond <dillonhammond@gmail.com>
2020-01-13 21:38:14 -0600

    added reference in MemRange and MemPosition

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -7,7 +7,7 @@ use crate::{
     },
 };
 
-use std::{collections::HashMap, num::NonZeroU32};
+use std::{collections::HashMap, num::NonZeroU32, fmt::Debug};
 
 pub struct Assembled {}
 
@@ -106,28 +106,34 @@ enum MemRangeStatus {
 
 impl Eq for MemRangeStatus {}
 
-#[derive(Copy, Clone, Debug)]
-struct MemRange {
+#[derive(Clone, Debug)]
+struct MemRange<'a, T> {
     lower: u32,
     upper: u32, // inclusive
     status: MemRangeStatus,
+    data: Option<&'a T>,
 }
 
-#[derive(Copy, Clone, Debug)]
-struct MemPosition {
+impl<'a, T> Copy for MemRange<'a, T> where T: Clone {}
+
+#[derive(Clone, Debug)]
+struct MemPosition<'a, T> {
     pub lower: Option<u32>,
     pub upper: Option<u32>, // inclusive
     pub size: u32,
+    pub data: Option<&'a T>,
 }
 
-impl MemPosition {
-    pub fn new(lower: Option<u32>, upper: Option<u32>, size: u32) -> MemPosition {
-        MemPosition { lower, upper, size }
+impl<'a, T> Copy for MemPosition<'a, T> where T: Clone {}
+
+impl<'a, T> MemPosition<'a, T> where T: Clone + Debug {
+    pub fn new(lower: Option<u32>, upper: Option<u32>, size: u32, data: Option<&T>) -> MemPosition<T> {
+        MemPosition { lower, upper, size, data }
     }
 }
 
-impl MemRange {
-    pub fn new(lower: u32, upper: u32, status: MemRangeStatus) -> MemRange {
+impl<'a, T> MemRange<'a, T> where T: Clone + Debug {
+    pub fn new(lower: u32, upper: u32, status: MemRangeStatus, data: Option<&T>) -> MemRange<T> {
         if lower > upper {
             panic!("lower > upper");
         }
@@ -135,6 +141,7 @@ impl MemRange {
             lower,
             upper,
             status,
+            data,
         }
     }
     pub fn set_status(&mut self, status: MemRangeStatus) {
@@ -143,7 +150,7 @@ impl MemRange {
     pub fn size_bytes(&self) -> u32 {
         self.upper - self.lower + 1
     }
-    pub fn shrink(mut self, bytes: NonZeroU32) -> (MemRange, MemRange) {
+    pub fn shrink<'b>(mut self, bytes: NonZeroU32) -> (MemRange<'a, T>, MemRange<'b, T>) {
         self.upper -= bytes.get();
         (
             self,
@@ -151,14 +158,15 @@ impl MemRange {
                 self.upper + 1,
                 self.upper + 1 + bytes.get(),
                 MemRangeStatus::Free,
+                None,
             ),
         )
     }
-    pub fn grow(
+    pub fn grow<'b>(
         mut self,
-        next: Option<&[MemRange]>,
+        next: Option<&'b [MemRange<'b, T>]>,
         bytes: NonZeroU32,
-    ) -> (MemRange, Option<Vec<MemRange>>) {
+    ) -> (MemRange<'a, T>, Option<Vec<MemRange<'b, T>>>) {
         self.upper += bytes.get();
         let mut r = None;
         if let Some(ranges) = next {
@@ -171,10 +179,10 @@ impl MemRange {
                     }
                     mem.upper < self.upper
                 })
-                .collect::<Vec<MemRange>>();
+                .collect::<Vec<MemRange<'b, T>>>();
             if tmp.len() > 0 {
                 let next = tmp[0];
-                tmp[0] = MemRange::new(self.upper + 1, next.upper, MemRangeStatus::Free);
+                tmp[0] = MemRange::new(self.upper + 1, next.upper, MemRangeStatus::Free, None);
             }
             r = Some(tmp);
         }
@@ -185,7 +193,7 @@ impl MemRange {
      * The first element exists if there is a MemRange lower than middle
      * The third element exists if there is a MemRange higher than middle
      */
-    pub fn insert(self, middle: MemRange) -> (Option<MemRange>, MemRange, Option<MemRange>) {
+    pub fn insert<'b, 'c, 'd>(self, middle: MemRange<'c, T>) -> (Option<MemRange<'b, T>>, MemRange<'c, T>, Option<MemRange<'d, T>>) {
         if MemRangeStatus::Free != self.status {
             panic!(
                 "Tried to insert into non-free block: {:?} <-> {:?}",
@@ -206,6 +214,7 @@ impl MemRange {
                     middle.upper + 1,
                     self.upper,
                     MemRangeStatus::Free,
+                    None,
                 )),
             )
         } else if middle.upper == self.upper {
@@ -214,6 +223,7 @@ impl MemRange {
                     self.lower,
                     middle.lower - 1,
                     MemRangeStatus::Free,
+                    None,
                 )),
                 middle,
                 None,
@@ -224,17 +234,19 @@ impl MemRange {
                     self.lower,
                     middle.lower - 1,
                     MemRangeStatus::Free,
+                    None,
                 )),
                 middle,
                 Some(MemRange::new(
                     middle.upper + 1,
                     self.upper,
                     MemRangeStatus::Free,
+                    None,
                 )),
             )
         }
     }
-    pub fn merge(mut self, other: MemRange) -> MemRange {
+    pub fn merge<'b>(mut self, other: MemRange<'b, T>) -> MemRange<'a, T> {
         if self.upper + 1 != other.lower {
             panic!("Is there a hole? {:?} <-> {:?}", self, other);
         }
@@ -247,7 +259,7 @@ impl MemRange {
         self.upper = other.upper;
         self
     }
-    fn first_fit_insert(position: MemPosition, memory: &mut Vec<MemRange>) {
+    fn first_fit_insert<'b>(position: MemPosition<'b, T>, memory: &mut Vec<MemRange<'b, T>>) {
         let l = position.lower.unwrap();
         let u = position.upper.unwrap();
         if let Some((i, m)) = memory
@@ -255,7 +267,7 @@ impl MemRange {
             .enumerate()
             .find(|(_, mem)| mem.lower <= l && u <= mem.upper && MemRangeStatus::Free == mem.status)
         {
-            let result = m.insert(MemRange::new(l, u, MemRangeStatus::Allocated));
+            let result = m.insert(MemRange::new(l, u, MemRangeStatus::Allocated, position.data));
             memory.remove(i);
             if let Some(x) = result.2 {
                 memory.insert(i, x);
@@ -271,16 +283,17 @@ impl MemRange {
             );
         }
     }
-    fn first_fit_insert_arbitray(size: u32, memory: &mut Vec<MemRange>) {
+    fn first_fit_insert_arbitray<'b>(position: MemPosition<'b, T>, memory: &mut Vec<MemRange<'b, T>>) {
         if let Some((i, m)) = memory
             .iter()
             .enumerate()
-            .find(|(_, mem)| MemRangeStatus::Free == mem.status && size <= mem.size_bytes())
+            .find(|(_, mem)| MemRangeStatus::Free == mem.status && position.size <= mem.size_bytes())
         {
             let result = m.insert(MemRange::new(
                 m.lower,
-                m.lower + size - 1,
+                m.lower + position.size - 1,
                 MemRangeStatus::Allocated,
+                position.data,
             ));
             memory.remove(i);
             if let Some(x) = result.2 {
@@ -293,18 +306,18 @@ impl MemRange {
         } else {
             panic!(
                 "Could not find placement for: {} bytes in {:?}",
-                size, memory
+                position.size, memory
             );
         }
     }
-    pub fn first_fit(positions: &[MemPosition], min: u32, max: u32) -> Vec<MemRange> {
-        let mut memory = vec![MemRange::new(min, max, MemRangeStatus::Free)];
-        let mut arbitraries: Vec<MemPosition> = Vec::new();
+    pub fn first_fit<'b>(positions: &'b [MemPosition<'b, T>], min: u32, max: u32) -> Vec<MemRange<'b, T>> {
+        let mut memory = vec![MemRange::new(min, max, MemRangeStatus::Free, None)];
+        let mut arbitraries: Vec<MemPosition<'b, T>> = Vec::new();
         positions.into_iter().for_each(|pos| match pos {
             MemPosition {
-                lower: Some(l),
-                upper: Some(u),
-                size,
+                lower: Some(_l),
+                upper: Some(_u),
+                ..
             } => {
                 MemRange::first_fit_insert(*pos, &mut memory);
             }
@@ -312,30 +325,28 @@ impl MemRange {
                 lower: Some(l),
                 upper: None,
                 size,
+                data,
             } => {
                 let u = l + size - 1;
-                let pos = MemPosition::new(Some(*l), Some(u), *size);
+                let pos = MemPosition::new(Some(*l), Some(u), *size, *data);
                 MemRange::first_fit_insert(pos, &mut memory);
             }
             MemPosition {
                 lower: None,
                 upper: Some(u),
                 size,
+                data,
             } => {
                 let l = u + 1 - size;
-                let pos = MemPosition::new(Some(l), Some(*u), *size);
+                let pos = MemPosition::new(Some(l), Some(*u), *size, *data);
                 MemRange::first_fit_insert(pos, &mut memory);
             }
-            MemPosition {
-                lower: None,
-                upper: None,
-                size,
-            } => {
+            _ => {
                 arbitraries.push(*pos);
             }
         });
         arbitraries.into_iter().for_each(|pos| {
-            MemRange::first_fit_insert_arbitray(pos.size, &mut memory);
+            MemRange::first_fit_insert_arbitray(pos, &mut memory);
         });
         memory
     }
@@ -350,7 +361,7 @@ fn layout_text_segment(
     text_segment_entries: &mut [TextSegment],
     labels: &mut HashMap<String, NonZeroU32>,
 ) {
-    let positions: Vec<MemPosition> = text_segment_entries.iter().map(|segment| {
+    let positions: Vec<MemPosition<TextSegment>> = text_segment_entries.iter().map(|segment| {
         let size_bytes = (segment.instructions.len() * 4) as u32;
         let mut lower = None;
         if let Some(start) = &segment.start_address {
