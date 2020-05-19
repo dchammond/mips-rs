a251bec07f94f2a0306fc9104f63c97f70e9d0c8
Dillon Hammond <dillonhammond@gmail.com>
2020-05-19 13:54:41 -0500

    expand label form li la

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -1,6 +1,6 @@
 use crate::{
     instructions::{itype::*, jtype::*, Inst},
-    machine::{address::Address, memory::*},
+    machine::{address::Address, memory::*, register::*},
     parser::parser::{
         DataBytes, DataCString, DataEntry, DataHalfs, DataSegment, DataSpace,
         DataWords, Parsed, TextSegment,
@@ -11,6 +11,16 @@ use crate::{
 use std::{collections::HashMap, convert::TryFrom, num::NonZeroU32};
 
 pub struct Assembled {}
+
+fn expand_li_la_label(rs: Reg, rt: Reg, label: Address) -> (ITypeLabel, ITypeLabel) {
+    let mut new_label_hi = label.label.unwrap().pop().unwrap();
+    let mut new_label_lo = new_label_hi.clone();
+    new_label_hi.push_str("@hi");
+    new_label_lo.push_str("@lo");
+    let lui = ITypeLabel::new(IInst::lui, rs, rt, Address::from(new_label_hi));
+    let ori = ITypeLabel::new(IInst::ori, rs, rt, Address::from(new_label_lo));
+    (lui, ori)
+}
 
 fn expand_pseudo(text_segments: Vec<TextSegment>) -> Vec<TextSegment> {
     let new_text_segments = Vec::with_capacity(text_segments.len());
@@ -28,13 +38,14 @@ fn expand_pseudo(text_segments: Vec<TextSegment>) -> Vec<TextSegment> {
                             rs,
                             rt,
                             label
+                        }) |
+                        Inst::ILabel(ITypeLabel {
+                            opcode: IInst::li,
+                            rs,
+                            rt,
+                            label
                         }) => {
-                            let mut new_label_hi = label.label.unwrap().pop().unwrap();
-                            let mut new_label_lo = new_label_hi.clone();
-                            new_label_hi.push_str("@hi");
-                            new_label_lo.push_str("@lo");
-                            let lui = ITypeLabel::new(IInst::lui, rs, rt, Address::from(new_label_hi));
-                            let ori = ITypeLabel::new(IInst::ori, rs, rt, Address::from(new_label_lo));
+                            let (lui, ori) = expand_li_la_label(rs, rt, label);
                             new_segment.instructions.push((addr, lui.into()));
                             new_segment.instructions.push((None, ori.into()));
                         },
@@ -51,5 +62,6 @@ pub type SymbolTable = HashMap<String, u32>;
 pub fn assemble(mut parsed: Parsed) {
     let mut labels: SymbolTable = HashMap::new();
     resolver::assign_addresses(&mut parsed, &mut labels);
+    println!("{:#?}", labels);
     //expand_pseudo(&mut parsed.text_segment);
 }
