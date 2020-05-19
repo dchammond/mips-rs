40aa4c248aae2bd83cebcbbc7298e3c783c04f17
Dillon Hammond <dillonhammond@gmail.com>
2020-05-19 13:35:49 -0500

    expand la

--- src/assembler/assembler.rs
+++ src/assembler/assembler.rs
@@ -12,21 +12,39 @@ use std::{collections::HashMap, convert::TryFrom, num::NonZeroU32};
 
 pub struct Assembled {}
 
-/*
-fn expand_pseudo(text_segments: &mut [TextSegment]) {
-    let insertions: Vec<((usize, usize), (Inst, Inst))> = Vec::new();
-    for (segment_idx, text_segment) in text_segments.iter().enumerate() {
-        for (instr_idx, instruction) in text_segment.instructions.iter().enumerate() {
-            match &instruction.1 {
-                Inst::ILabel(ITypeLabel{opcode: IInst::la, rs, rt, label}) => {
-
-                },
-                _ => (),
-            }
-        }
-    }
+fn expand_pseudo(text_segments: Vec<TextSegment>) -> Vec<TextSegment> {
+    let new_text_segments = Vec::with_capacity(text_segments.len());
+    text_segments
+        .into_iter()
+        .for_each(|segment| {
+            let mut new_segment = TextSegment::new();
+            new_segment.start_address = segment.start_address;
+            segment.instructions
+                .into_iter()
+                .for_each(|(addr, inst)| {
+                    match inst {
+                        Inst::ILabel(ITypeLabel {
+                            opcode: IInst::la,
+                            rs,
+                            rt,
+                            label
+                        }) => {
+                            let mut new_label_hi = label.label.unwrap().pop().unwrap();
+                            let mut new_label_lo = new_label_hi.clone();
+                            new_label_hi.push_str("@hi");
+                            new_label_lo.push_str("@lo");
+                            let lui = ITypeLabel::new(IInst::lui, rs, rt, Address::from(new_label_hi));
+                            let ori = ITypeLabel::new(IInst::ori, rs, rt, Address::from(new_label_lo));
+                            new_segment.instructions.push((addr, lui.into()));
+                            new_segment.instructions.push((None, ori.into()));
+                        },
+                        //Inst::JLabel() => ,
+                        _ => new_segment.instructions.push((addr, inst)),
+                    }
+                });
+        });
+    new_text_segments
 }
-*/
 
 pub type SymbolTable = HashMap<String, u32>;
 
